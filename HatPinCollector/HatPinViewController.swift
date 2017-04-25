0c9bad17b3ffc691388de1ae304fbb6993c6572a
Jordan Matthews <hayleywoodard@Hayleys-MacBook-Pro.local>
2017-04-25 16:48:16 -0400

    Outlets and Actions created for HatPinVC

---
+++ HatPinCollector/HatPinViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HatPinViewController.swift
+//  HatPinCollector
+//
+//  Created by Hayley Woodard on 4/25/17.
+//  Copyright © 2017 Jordan Matthews. All rights reserved.
+//
+
+import UIKit
+
+class HatPinViewController: UIViewController {
+    
+    
+    @IBOutlet weak var pinImageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+}
