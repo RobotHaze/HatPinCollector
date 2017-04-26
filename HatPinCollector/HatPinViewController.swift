079f0879e62a5790545379959aa4156d0b25270c
Jordan Matthews <hayleywoodard@hayleys-mbp.attlocal.net>
2017-04-26 15:05:40 -0400

    Made photos button functional

--- HatPinCollector/HatPinViewController.swift
+++ HatPinCollector/HatPinViewController.swift
@@ -8,24 +8,44 @@
 
 import UIKit
 
-class HatPinViewController: UIViewController {
+class HatPinViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     @IBOutlet weak var pinImageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
         
     }
     
     
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        pinImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
     @IBAction func cameraTapped(_ sender: Any) {
     }
+    
+    
     @IBAction func addTapped(_ sender: Any) {
     }
     
